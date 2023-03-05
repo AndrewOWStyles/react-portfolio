f03a35c4068a5409b0669af7c435d5e843b01355
Andrew Styles <andystyles@hotmail.co.uk>
2023-03-05 19:14:54 +0000

    added grid for navbar to be on the side of the page as well as styling for the home page

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavTabs from './components/NavTabs';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Blog from './components/pages/Blog';
+import Contact from './components/pages/Contact';
+import './app.css'
+
+
+
+function App() {
+  return (
+    <Router basename='react-portfolio'>
+      <div className='gridContainer'>
+        <div>
+          <NavTabs/>
+        </div>
+        <div>
+          <Routes >
+            {/* Define routes using the Route component to render different page components at different paths */}
+            {/* Define a default route that will render the Home component */}
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="blog" element={<Blog />} />
+            {/* Define a route that will have descendant routes */}
+            <Route path="contact/*" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+      </Router>
+  );
+}
+
+
+export default App;
+
---
+++ src/components/NavTabs.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './styles/navTabs.css'
+
+function NavTabs() {
+  return (
+    <div className='navDiv'>
+      <ul className="navTabs">
+        <li className="nav-item">
+          <NavLink
+            to="/"
+            end
+            className={({ isActive }) =>
+              isActive ? 'nav-link active' : 'nav-link'
+            }
+          >
+            Home
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="about"
+            className={({ isActive }) =>
+              isActive ? 'nav-link active' : 'nav-link'
+            }
+          >
+            About
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="blog"
+            className={({ isActive }) =>
+              isActive ? 'nav-link active' : 'nav-link'
+            }
+          >
+            Blog
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="contact"
+            end
+            className={({ isActive }) =>
+              isActive ? 'nav-link active' : 'nav-link'
+            }
+          >
+            Contact
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="contact/learn"
+            className={({ isActive }) =>
+              isActive ? 'nav-link active' : 'nav-link'
+            }
+          >
+            Learn
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavTabs;
